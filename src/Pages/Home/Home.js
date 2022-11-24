0ba26353ff3e5fe397bc87d091be7f9abf6d5942
Avijit-Sarkar-Avi <avijit215054@gmail.com>
2022-11-24 10:04:53 +0600

    added section element

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import useTitle from '../../Hooks/useTitle';
 import bannerImage from '../../asset/banner.jpg'
+import stripeImage from '../../asset/stripe.jpg'
 
 const Home = () => {
     useTitle('Home')
@@ -16,7 +17,21 @@ const Home = () => {
                             <p>We easy to your sell your product at a good price </p>
                             <p> The Buyer get the best product in his limited price.</p>
                         </p>
+                    </div>
+                </div>
+            </div>
 
+            <div>
+                Categories
+            </div>
+
+            <div className="hero ">
+                <div className="hero-content flex-col lg:flex-row">
+                    <img src={stripeImage} alt="" className="w-96 h-52 rounded-lg shadow-2xl" />
+                    <div>
+                        <h1 className="text-5xl font-bold">Get 20% Discount</h1>
+                        <p className="py-6">Payment by Stripe and get 20% discount for every product.</p>
+                        <button className="btn btn-primary">Happy Purchase</button>
                     </div>
                 </div>
             </div>
