5151b15af959b1826782a3b6c69c86d54c124406
Avijit-Sarkar-Avi <avijit215054@gmail.com>
2022-11-25 22:23:18 +0600

    added jquery

--- src/Pages/Home/Categories/Categories.js
+++ src/Pages/Home/Categories/Categories.js
@@ -1,30 +1,46 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
-import ArduinoImage from '../../../asset/arduinoImage.jpg'
-import RaspberrypiImage from '../../../asset/raspberrypiImage.jpg'
-import AvrImage from '../../../asset/avrImage.jpg'
-import Category from './Category';
+import Category from './Category'
 
 const Categories = () => {
-    const cardData = [
-        {
-            id: 1,
-            name: 'Arduino',
-            description: 'We have good and fresh quality of Arduino circuit.',
-            icon: ArduinoImage
-        },
-        {
-            id: 2,
-            name: 'RaspberryPi',
-            description: 'RaspberryPI that can easyly connect with pc by bluetooth and good quality.',
-            icon: RaspberrypiImage
-        },
-        {
-            id: 3,
-            name: 'AVR',
-            description: 'We have good and fresh quality of AVR microcontroller.',
-            icon: AvrImage
-        },
-    ]
+
+    // const [cardData, setCardData] = useState([]);
+
+    const { data: cardData = [] } = useQuery({
+        queryKey: ['cardData'],
+        queryFn: async () => {
+            const res = await fetch('http://localhost:5000/cardData');
+            const data = await res.json();
+            return data
+        }
+    })
+
+    // useEffect(() => {
+    //     fetch('http://localhost:5000/cardData')
+    //         .then(res => res.json())
+    //         .then(data => setCardData(data))
+    // }, [])
+
+    // const cardData = [
+    //     {
+    //         id: 1,
+    //         name: 'Arduino',
+    //         description: 'We have good and fresh quality of Arduino circuit.',
+    //         icon: "https://i.ibb.co/pZMGLtm/arduino-Image.webp"
+    //     },
+    //     {
+    //         id: 2,
+    //         name: 'RaspberryPi',
+    //         description: 'RaspberryPI that can easyly connect with pc by bluetooth and good quality.',
+    //         icon: "https://i.ibb.co/gyBv4jC/raspberrypi-Image.png"
+    //     },
+    //     {
+    //         id: 3,
+    //         name: 'AVR',
+    //         description: 'We have good and fresh quality of AVR microcontroller.',
+    //         icon: "https://i.ibb.co/qnnps8h/avrImage.png"
+    //     },
+    // ]
     return (
         <div className='grid lg:grid-cols-3 gap-10'>
 
